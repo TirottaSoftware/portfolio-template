a2284ebcccf0ad84e744385524a827842ceb2857
Cristian Tirotta <59051220+TirottaSoftware@users.noreply.github.com>
2022-07-08 10:45:27 +0300

    Testimonials Section - Finished

---
+++ assets/js/jquery-slider.js
@@ -0,0 +1,10 @@
+$('.testimonials-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    speed: 1000,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    infinite: true
+})
